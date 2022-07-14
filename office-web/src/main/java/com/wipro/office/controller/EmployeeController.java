621dde4d71502b57f45cbc0715e4182f27f6b6fe
gouravkumardodia <gouravkumardodia@gmail.com>
2022-07-14 10:33:55 +0530

    14July

--- office-web/src/main/java/com/wipro/office/controller/EmployeeController.java
+++ office-web/src/main/java/com/wipro/office/controller/EmployeeController.java
@@ -5,52 +5,62 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.wipro.office.entity.Employee;
 import com.wipro.office.repository.EmployeeRepository;
 import com.wipro.office.response.ApiResponse;
+import com.wipro.office.service.EmployeeService;
 
 @RestController
 @RequestMapping("/employee")
 public class EmployeeController 
 {
 	@Autowired
-	private EmployeeRepository empRepo;
+	private EmployeeService empService;
 	
 	@PostMapping("/save")
 	public ApiResponse saveEmployee(@RequestBody Employee emp)
 	{
-		try {
-			empRepo.save(emp);
-			return new ApiResponse(true, "Employee Saved Successfully !");
-		}catch(Exception ex) {
-			return new ApiResponse(false, "Employee Not Saved !");
-		}
+		ApiResponse response = empService.saveEmployee(emp);
+		return response;
 	}	
 	
 	@GetMapping("/list")
 	public ApiResponse listEmployee()
 	{
-		List<Employee> list = empRepo.findAll();
-		return new ApiResponse(true, list);
+		ApiResponse response = empService.listEmployees();
+		return response;
 	}
 	
 	// PathVariable
 	@GetMapping("/get/{empid}")
 	public ApiResponse getEmployee(@PathVariable(name = "empid") Integer empId) 
 	{
-		Optional<Employee> op = empRepo.findById(empId);
-		if(op.isPresent()) {
-			return new ApiResponse(true, op.get());
-		}else {
-			return new ApiResponse(false, "No Employee Found !");
-		}
+		ApiResponse response = empService.get(empId);
+		return response;
+	}
+	
+	@PutMapping("/update")
+	public ApiResponse updateEmployee(@RequestBody Employee emp)
+	{
+		ApiResponse response = empService.updateEmployee(emp);
+		return response;
+	}	
+	
+	@DeleteMapping("/delete/{empid}")
+	public ApiResponse deleteEmployee(@PathVariable(name = "empid") Integer empId) 
+	{
+		ApiResponse response = empService.deleteEmployee(empId);
+		return response;
 	}
 }
 
