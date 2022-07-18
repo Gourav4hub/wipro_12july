c8597f56d68b6a3b51e3fb77affd1a368299ad5d
gouravkumardodia <gouravkumardodia@gmail.com>
2022-07-18 12:30:01 +0530

    18July

--- office-web-2/src/main/java/com/wipro/office2/entity/Employee.java
+++ office-web-2/src/main/java/com/wipro/office2/entity/Employee.java
@@ -1,11 +1,14 @@
 package com.wipro.office2.entity;
 
 
+import java.util.List;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
@@ -44,6 +47,9 @@ public class Employee
 	@OneToOne
 	@JoinColumn(name = "address")
 	private Address address;
+	
+	@OneToMany(mappedBy = "employee")
+	private List<SalaryRecord> salaryRecords;
 
 	public Employee(EmployeeModel emp) {
 		this.empName = emp.getEmpName();
